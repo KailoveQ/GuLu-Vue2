b85de56fe833d37d0bacf7af83dcae397662694f
Kai <1114366961@qq.com>
2021-03-08 11:48:23 +0800

    绑定class的对象语法+CSS实现 button 的icon 位置

--- src/button.vue
+++ src/button.vue
@@ -1,19 +1,12 @@
 <template>
-  <button v-if="iconPosition==='right'" class="g-button">
-    <slot></slot>
-    <svg v-if="icon" class="icon" aria-hidden="true">
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
       <use :xlink:href="`#i-${icon}`"></use>
     </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
-
-  <button v-else class="g-button">
-
-    <svg v-if="icon" class="icon" aria-hidden="true">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
-    <slot></slot>
-  </button>
-
 </template>
 <script>
 export default {
@@ -28,14 +21,42 @@ export default {
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    vertical-align: middle;
+
     &:hover {
       border-color: var(--border-color-hover);
     }
+
     &:active {
       background-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .1em;
+    }
+
+    &.icon-right {
+      > .content {
+        order: 1;
+      }
+
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+      }
     }
   }
 </style>
