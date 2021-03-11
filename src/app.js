0fc7ff727d2b2c7327170f7263b1ee9df4054d24
Kai <1114366961@qq.com>
2021-03-11 10:49:25 +0800

    优化三种动画

--- src/app.js
+++ src/app.js
@@ -42,17 +42,19 @@ new Vue({
   created() {
   },
   methods: {
-    showToast() {
+    showToast1() {
+      this.showToast('top')
+    },
+    showToast2() {
+      this.showToast('middle')
+    },
+    showToast3() {
+      this.showToast('bottom')
+    },
+    showToast(position) {
       this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
-        position: 'bottom',
+        position,
         enableHtml: false,
-        closeButton: {
-          text: '已充值',
-          callback() {
-            console.log('他说已经充值智商了')
-          }
-        },
-        autoClose: false,
         autoCloseDelay: 3
       })
     }
