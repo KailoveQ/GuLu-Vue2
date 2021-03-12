dc9db65dcf08e8e023a4c3da7b24dfb8cb2244c5
Kai <1114366961@qq.com>
2021-03-12 19:06:08 +0800

    添加 single 选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="collapse">
+  <div className="collapse">
     <slot></slot>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-  name: 'GuluCollapse'
+  name: 'GuluCollapse',
+  props: {
+    single: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      eventBus: new Vue()
+    }
+  },
+  provide() {
+    if (this.single) {
+      return {
+        eventBus: this.eventBus
+      }
+    }
+  }
 }
 </script>
 
