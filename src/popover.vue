bee1f1819d5dfca1653f56178e82b30b46628e49
Kai <1114366961@qq.com>
2021-03-12 10:26:56 +0800

    添加插槽的 close API,使用slot-scope给插槽使用者活动组件的方法

--- src/popover.vue
+++ src/popover.vue
@@ -2,7 +2,7 @@
   <div class="popover" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible"
          :class="{[`position-${position}`]:true}">
-      <slot name="content"></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
@@ -13,6 +13,22 @@
 <script>
 export default {
   name: 'GuluPopover',
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+      }
+    },
+    trigger: {
+      type: String,
+      default: 'click',
+      validator(value) {
+        return ['click', 'hover'].indexOf(value) >= 0
+      }
+    }
+  },
   data() {
     return {
       visible: false,
@@ -50,22 +66,7 @@ export default {
       }
     }
   },
-  props: {
-    position: {
-      type: String,
-      default: 'top',
-      validator(value) {
-        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
-      }
-    },
-    trigger: {
-      type: String,
-      default: 'click',
-      validator(value) {
-        return ['click', 'hover'].indexOf(value) >= 0
-      }
-    }
-  },
+
   methods: {
     positionContent() {
       const {contentWrapper, triggerWrapper} = this.$refs
