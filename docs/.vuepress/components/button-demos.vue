35f421f343c31c87d86e29d87ba8d91654a5ab57
Kai <1114366961@qq.com>
2021-03-13 19:14:48 +0800

    文档写好了，但是需要尝试使用yarn

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,30 +1,26 @@
 <template>
-  <div>
+  <div class="button-wrapper">
     <g-button>默认按钮</g-button>
     <g-button icon="settings">默认按钮</g-button>
+    <g-button icon="settings" icon-position="right">默认按钮</g-button>
     <g-button :loading="true">默认按钮</g-button>
     <g-button disabled>默认按钮</g-button>
-
-    <pre>
-    <code>{{ content }}</code>
-    </pre>
   </div>
 </template>
+
 <script>
 import Button from '../../../src/button'
+
 export default {
   components: {
-    'g-button': Button
+    'g-button': Button,
   },
-  data() {
-    return {
-      content: `
-        <g-button>默认按钮</g-button>
-        <g-button icon="settings">默认按钮</g-button>
-        <g-button :loading="true">默认按钮</g-button>
-        <g-button disabled>默认按钮</g-button>
-      `
-    }
-  }
 }
+
 </script>
+
+<style lang="scss" scoped>
+.button-wrapper {
+  padding: 0 10px;
+}
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  title: '轱辘UI',
-  description: '一个好用的UI框架',
+  base: '/init-ui/',
+  title: 'Init UI',
+  description: '一个好用的 UI 框架',
   themeConfig: {
+    displayAllHeaders: true,
     nav: [
       {text: '主页', link: '/'},
-      {text: '文档', link: '/guide/'},
-      {text: '交流', link: 'https://google.com'},
+      {text: '文档', link: '/install/'},
+      {text: '交流', link: 'https://github.com/wh2887/init-ui/issues'},
     ],
     sidebar: [
       {
         title: '入门',
+        collapsable: false, // 可选的, 默认值是 true,
         children: [
           '/install/',
           '/get-started/',
@@ -17,9 +20,18 @@ module.exports = {
       },
       {
         title: '组件',
-        children: ['/components/button']
-      },
-
-    ]
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 0,    // 可选的, 默认值是 1
+        children: [
+          '/components/button',
+          '/components/input',
+          '/components/grid',
+          '/components/layout',
+          '/components/tabs',
+          '/components/popover',
+          '/components/toast',
+        ],
+      }
+    ],
   }
 }
