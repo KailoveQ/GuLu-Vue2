50cb16ef9e730aaaadd25460981bd2a320f35d85
Kai <1114366961@qq.com>
2021-03-29 19:31:23 +0800

    导出，发布npm 包

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
+import Col from './src/col'
+import Collapse from './src/collapse'
+import CollapseItem from './src/collapse-item'
+import Content from './src/content'
+import Footer from './src/footer'
+import Header from './src/header'
+import Input from './src/input'
+import Layout from './src/layout'
+import Popover from './src/popover'
+import Row from './src/row'
+import Sider from './src/sider'
+import Tabs from './src/tabs'
+import TabsBody from './src/tabs-body'
+import TabsHead from './src/tabs-head'
+import TabsItem from './src/tabs-item'
+import TabsPane from './src/tabs-pane'
+import Toast from './src/toast'
+import plugin from './src/plugin'
 
+export {
+  Button, ButtonGroup, Icon, Col, Collapse, CollapseItem, Content, Footer, Header, Input, Layout, Popover
+  , Row, Sider, Tabs, TabsHead, TabsBody, TabsItem, TabsPane, Toast,plugin
+}
 
-  export {ButtonGroup, Button, Icon}
